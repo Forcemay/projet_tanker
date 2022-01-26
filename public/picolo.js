dfbcc9725ea157a77604bb5378ee4974f50c5ce0
Mayeul Forceville <mayeul.forceville-ext@conti-engineering.com>
2022-01-27 00:26:09 +0100

    Recover true false game for picolo
    Signed-off-by: Mayeul Forceville <mayeul.forceville-ext@conti-engineering.com>

--- public/picolo.js
+++ public/picolo.js
@@ -1,14 +1,17 @@
-var questions = ["Never have I ever gone skinny-dipping",
-"Never have I ever had sex in public",
-"Never have I ever tried to enter the wrong house while being drunk",
-"Never have I ever had sex during my period",
-"Never have I ever been drunk at work",
-"Never have I ever had anal sex",
-"Never have I ever been high around my family",
-"Never have I ever re-gifted a present",
-"Never have I ever lied about seeing my friend s ex behind their back",
-"Never have I ever photoshopped a picture of myself"
+var questions = ["Never have I ever gone skinny-dipping/0",
+"Never have I ever had sex in public/1",
+"Never have I ever tried to enter the wrong house while being drunk/0",
+"Never have I ever had sex during my period/1",
+"Never have I ever been drunk at work/0",
+"Never have I ever had anal sex/1",
+"Never have I ever been high around my family/0",
+"Never have I ever re-gifted a present/1",
+"Never have I ever lied about seeing my friend s ex behind their back/0",
+"Never have I ever photoshopped a picture of myself/1"
 ];
+
+var response = 1;
+
 function sleep(ms) {
       return new Promise(resolve => setTimeout(resolve, ms));
    }
@@ -23,9 +26,10 @@ function go_back_home(){
 function plot_question(){
   var index = Math.floor(Math.random()*(questions.length));
   var item = questions[index];
-
+  var question = item.split('/')[0];
+  response = item.split('/')[1];
   if (item) {
-     document.getElementById('question').innerHTML = item ;
+     document.getElementById('question').innerHTML = question ;
      if (index > -1) {
         questions.splice(index, 1);
     }
@@ -44,14 +48,73 @@ function send_drink(){
             document.location.href = "/index.html";
         }
         else {
-        drink_button_off();}
-
+        button_reset("true_id");
+        button_reset("false_id");
+        plot_question();}
     }
     }
     xhr.send(JSON.stringify({
     drink: 1
     }));
-    drink_button_on();
+}
+
+async function true_response(){
+    control_drink("inactive","true_id");
+    control_drink("inactive","false_id");
+    if (response == 1) {
+        button_green("true_id");
+        await sleep(2000);
+        button_reset("true_id");
+        button_reset("false_id");
+        plot_question();
+    }
+    else {
+        button_red("true_id");
+        send_drink();
+    }
+}
+
+async function false_response(){
+    control_drink("inactive","true_id");
+    control_drink("inactive","false_id");
+    if (response == 0) {
+        button_green("false_id");
+        await sleep(2000);
+        button_reset("true_id");
+        button_reset("false_id");
+        plot_question();
+    }
+    else {
+        button_red("false_id");
+        send_drink();
+    }
+}
 
 
+function button_red(id){
+  var button = document.getElementById(id);
+  button.style.backgroundColor = "black";
+  button.style.color = "red";
+}
+
+function button_green(id){
+  var button = document.getElementById(id);
+  button.style.backgroundColor = "black";
+  button.style.color = "green";
+}
+
+function button_reset(id){
+  var button = document.getElementById(id);
+  button.style.backgroundColor = "transparent";
+  button.style.color = "black";
+  control_drink("active",id)
+}
+function control_drink(state,id){
+  let button = document.getElementById(id);
+  if (state=="active"){
+    button.disabled = false;
+  }
+  else {
+    button.disabled = true;
+  }
 }
